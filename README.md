1a9c968d00829efb9515dd238dc2d0e9946dc353
Serra Allgood <serra.allgood@procore.com>
2019-05-11 18:06:55 -0700

    Final commit

---
+++ README.md
@@ -0,0 +1,9 @@
+domain_gen
+==========
+
+An exercise in writing a simple concurrent Rust cli tool.
+`domain_gen` takes in a list of words and a list of top-level domains,
+and generates possible domains with the TLD acting as the end of the word.
+
+Concurrent code is based on studying [ranagrams](https://github.com/dfhoughton/ranagrams),
+and as such, this project is similarly licensed under the GPLv2.
---
+++ src/main.rs
@@ -0,0 +1,102 @@
+extern crate num_cpus;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::fs::File;
+use std::io::Read;
+use std::process;
+
+fn main() {
+    let cpus = num_cpus::get();
+    let words = Arc::new(read_file("words.txt"));
+    let tlds = Arc::new(read_file("tld.txt"));
+
+    let words_per_thread = words.len() / cpus;
+    let mut handles = Vec::with_capacity(cpus);
+    let domains = Arc::new(Mutex::new(Vec::with_capacity(words.len())));
+
+    for i in 0..cpus {
+        let start = i * words_per_thread;
+        let end = (i + 1) * words_per_thread - 1;
+        let slice = Arc::clone(&words);
+        let tlds = Arc::clone(&tlds);
+        let domains = Arc::clone(&domains);
+
+        let handle = thread::spawn(move || {
+            for word in slice[start..end].iter() {
+                for tld in tlds.iter() {
+                    if word_too_small(&word, &tld) {
+                        continue;
+                    }
+
+                    if word_ends_in_tld(&word, &tld) {
+                        push_domain(Arc::clone(&domains), &word, &tld);
+                    } else if possible_plural(&tld) && word_ends_in_tld(&word, &tld[0..(tld.len() - 1)]) {
+                        push_domain(Arc::clone(&domains), &word, &tld);
+                    }
+                }
+            }
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    domains.lock().unwrap().sort_unstable();
+    domains.lock().unwrap().dedup();
+
+    for domain in domains.lock().unwrap().iter() {
+        println!("{}", domain);
+    }
+}
+
+fn possible_plural(tld: &str) -> bool {
+    &tld[(tld.len() - 1)..tld.len()] == "s"
+}
+
+fn push_domain(domains: Arc<Mutex<Vec<String>>>, word: &str, tld: &str) {
+    let mut domain = String::with_capacity(word.len() + 1);
+    for (i, chr) in word.chars().enumerate() {
+        domain.push(chr);
+        if word.len() - tld.len() - 1 == i {
+            domain.push('.');
+        }
+    }
+
+    domains.lock().unwrap().push(domain);
+}
+
+fn word_ends_in_tld(word: &str, tld: &str) -> bool {
+    let (start, end) = start_end(word, tld);
+    &word[start..end] == tld
+}
+
+fn word_too_small(word: &str, tld: &str) -> bool {
+    word.len() <= tld.len()
+}
+
+fn start_end(word: &str, tld: &str) -> (usize, usize) {
+    let start = word.len() - tld.len();
+    let end = word.len();
+    (start, end)
+}
+
+fn read_file(name: &str) -> Vec<String> {
+    let mut strings = String::new();
+    let mut file = match File::open(name) {
+        Err(_) => {
+            eprintln!("Failed to open {}", name);
+            process::exit(1);
+        }
+        Ok(file) => file
+    };
+    match file.read_to_string(&mut strings) {
+        Err(why) => {
+            eprintln!("Failed to read {}: {}", name, why);
+            process::exit(1);
+        }
+        Ok(_) => ()
+    }
+    strings.lines().map(|s| String::from(s)).collect::<Vec<String>>()
+}
